d1f6ba7d8d571c89cd9e19da2f624a7548a5082b
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-03-06 10:11:00 +0100

    adds a header and a searchbar component to see if it works as it should

---
+++ elins-ca/src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header className="header">
+		<h1>THIS IS THE HEADER</h1>
+		<p>The header must have a navbar + a cart icon</p>
+		</header>
+	)
+}
---
+++ elins-ca/src/Searchbar.jsx
@@ -0,0 +1,5 @@
+export default function Searchbar() {
+	return (
+		<p>This is the searchbar</p>
+	)
+}
